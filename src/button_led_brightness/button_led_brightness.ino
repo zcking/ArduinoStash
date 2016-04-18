2bf486a000366879028c84aed4b48382e1534612
Zachary King <kingzach77@gmail.com>
2016-04-18 01:23:21 -0500

    Button for changing LED brightness

---
+++ src/button_led_brightness/button_led_brightness.ino
@@ -0,0 +1,45 @@
+/*
+ * Simple Button & LED 
+ * This program uses a button to 
+ * change an LED light's brightness
+ */
+
+const int switchPin = 8;
+const int ledPin = 11;
+
+bool lastPress = LOW;
+bool currentButton = LOW;
+int ledLevel = 0;
+
+void setup()
+{
+  pinMode(switchPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+// Fixes the issue with "bouncing" of the switch
+bool debounce(bool last)
+{
+  bool current = digitalRead(switchPin);
+  if (last != current)
+  {
+    delay(5);
+    current = digitalRead(switchPin);
+  }
+  return current;
+}
+
+void loop()
+{
+  currentButton = debounce(lastButton);
+
+  // If the switch/button has been pressed, toggle the ledOn variable
+  if (lastButton == LOW && currentButton == HIGH)
+  {
+    ledLevel = (ledLevel + 51) % 255; // Add 51 units of brightness level, limiting to 255
+  }
+
+  lastButton = currentButton;
+  analogWrite(ledPin, ledLevel);
+}
+

9717ad60c654e1239e356849d3046184bd3e67b2
Zachary King <kingzach77@gmail.com>
2016-04-18 17:08:32 -0500

    Varying Nightlight

---
+++ src/varying_nightlight/varying_nightlight.ino
@@ -0,0 +1,25 @@
+const int sensorPin = 0;
+const int ledPin = 9;
+
+int value = 0;
+int ledLevel = 0;
+
+void setup()
+{
+  analogReference(DEFAULT); // not necessary
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600); 
+}
+
+void loop()
+{
+  value = analogRead(sensorPin);
+
+  value = constrain(value, 750, 900); // if < 750, value = 750; if > 900, value = 900
+  ledLevel = map(value, 750, 900, 255, 0); // map 750 -> 255 and 900 -> 0
+  
+  // Serial.println(value); 
+  
+  analogWrite(ledPin, ledLevel);
+}
+

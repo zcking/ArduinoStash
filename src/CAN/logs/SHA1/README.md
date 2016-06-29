cb9bf446c69fabd586a4cd5b17a4422782806ece
Zachary King <kingzach77@gmail.com>
2016-06-29 10:42:45 -0500

    Measured time to fully create the Authentication Messages

--- src/CAN/logs/SHA1/README.md
+++ src/CAN/logs/SHA1/README.md
@@ -36,5 +36,8 @@ with different delay values to see if they yield different results.
 ---
 
 ### Time Measurement of Authentication
-The average time it took to authenticate a CAN message was 3973.04 microseconds,
-or 3.97304 milliseconds.
+The average time it took to write the HMAC data (not actually created the HMAC), 
+was 3973.04 microseconds, or 3.97304 milliseconds.  
+
+### Time Measurement to Create Full Authentication Messages was 15,443.0204 microseconds, 
+or 15.44302 milliseconds.
---
+++ src/CAN/plot_log_custom.py
@@ -0,0 +1,58 @@
+"""
+File: plot_log.py
+Author: Zachary King
+6/22/2016
+---
+Plots data from log files produced by 
+the CAN Arduino programs.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+minimx = 999
+maximx = 0
+minimy = 999
+maximy = 0
+
+average = 0
+total = 0
+counter = 0
+
+def plot_data(file_path):
+    global minimx, minimy, maximx, maximy, average, total, counter
+    
+    with open(file_path, 'r') as f:
+        entries = f.read().splitlines() # read all the lines into a list
+        start = float(entries[0].split(':')[0]) # get first second
+        end = float(entries[-1].split(':')[0]) # get last second
+
+        counter += (end - start)
+        values = []
+        for entry in entries:
+            values.append(int(entry.split(':')[1]))
+            total += (int(entry.split(':')[1]))
+        average = total / counter
+
+        minimx = min(minimx, start)
+        maximx = max(maximx, end)
+        minimy = min(minimy, min(values))
+        maximy = max(maximy, max(values))
+
+        plt.plot(values)
+        plt.axis([minimx, maximx, minimy+2000, maximy+1500])
+        
+
+if __name__ == '__main__':
+    if len(sys.argv) < 5:
+        print('Usage: python plot_log.py <logfile-path> <title> <x-axis-label> <y-axis-label>')
+        sys.exit(1)
+    else:
+        plot_data(sys.argv[1])
+        plt.title(sys.argv[2])
+        plt.xlabel(sys.argv[3])
+        plt.ylabel(sys.argv[4])
+
+    print(average)
+    plt.show()
